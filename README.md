f0ba85c67aa68d78da87eb399f7ace29ffec3b74
kuotient <kuotient.dev@gmail.com>
2024-03-27 18:43:00 +0900

    Add mt-bench-ko benchmark

--- README.md
+++ README.md
@@ -4,12 +4,18 @@
 ## Benchmark Website
 https://lk.instruct.kr/
 
+## Current Benchmarks
+| 이름 | # | 데이터 | 출처 |
+|---|---|---|---|
+| `logic-kor` | 42 | 한국어 언어모델 다분야 사고력 벤치마크 | instruct-kr |
+| `mt-bench-ko` | 80 | [MT-BENCH](https://github.com/lm-sys/FastChat/tree/main/fastchat/llm_judge)의 한국어 번역, 검수 벤치마크 데이터셋 | [kuotient/FastChat](https://github.com/lm-sys/FastChat/tree/main/fastchat/llm_judge) |
+
 ## Repository
-본 Repo는 LogicKor 벤치마크의 추론 및 평가 코드, 데이터셋을 담고 있습니다.
+본 Repo는 LogicKor를 포함한 다양한 다분야 사고력 벤치마크의 추론 및 평가 코드, 데이터셋을 담고 있습니다.
 
 ## Evalutation Example
-EEVE 템플릿, GPU 0,1 사용, model_len 4096
+EEVE 템플릿, GPU 0,1 사용, model_len 4096, logic-kor 벤치마크
 ```
-python generator.py --model yanolja/EEVE-Korean-Instruct-10.8B-v1.0 --template templates/template-EEVE.json --gpu_devices 0,1 --model_len 4096
+python generator.py --model yanolja/EEVE-Korean-Instruct-10.8B-v1.0 --template templates/template-EEVE.json --bench_name logic-kor --gpu_devices 0,1 --model_len 4096
 python judgement.py --model-output yanolja_EEVE-Korean-Instruct-10.8B-v1.0.jsonl --openai-api-key sk-somethingsomething --threads 30
 ```
--- generator.py
+++ generator.py
@@ -8,6 +8,7 @@ parser.add_argument('--gpu_devices', help=' : CUDA_VISIBLE_DEVICES', default='0'
 parser.add_argument('--model', help=' : Model to evaluate', default='yanolja/EEVE-Korean-Instruct-2.8B-v1.0')
 parser.add_argument('--template', help=' : Template File Location', default='./templates/template-EEVE.json')
 parser.add_argument('--model_len', help=' : Maximum Model Length', default=4096, type=int)
+parser.add_argument('--bench_name', help=' : Benchmark to evaluate', default='logic-kor')
 args = parser.parse_args()
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_devices
 gpu_counts = len(args.gpu_devices.split(','))
@@ -19,7 +20,8 @@ double_turn_template = df_config.iloc[1]
 llm = LLM(model=args.model, tensor_parallel_size=gpu_counts, max_model_len=args.model_len, gpu_memory_utilization=0.95)
 sampling_params = SamplingParams(temperature=0, top_p=1, top_k=-1, early_stopping=True, best_of=4, use_beam_search=True, skip_special_tokens=False, max_tokens=args.model_len, stop=['<|endoftext|>', '</s>', '<|im_end|>', '[INST]', '[/INST]'])
 
-df_questions = pd.read_json('questions.jsonl', lines=True)
+question_file = f"questions/{args.bench_name}.jsonl"
+df_questions = pd.read_json(question_file, lines=True)
 
 def format_single_turn_question(question):
     return single_turn_template.format(question[0])
